4ae26d4795b4b335d9faf9ff311692772f192508
mfukechristian <mfukechristian@gmail.com>
2025-03-04 11:05:14 +0200

    Add user authentication and management features with JWT and MongoDB

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 // Load environment variables
 dotenv.config();
 
+// Connect to MongoDB
+connectDB();
+
+// Create Express app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Define routes
 app.get("/", (req, res) => {
   res.send("CodeHub Backend is running!");
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
